361f3387767d688be3cabfb5de6cf2c058e8175f
acer NITRO 5 <feruzbekhamrayev2002@gmail.com>
2024-08-22 01:11:56 +0500

    Yangilangan background va buttonlar qismi o'zgartirildi

--- src/siteBody/dashboard.jsx
+++ src/siteBody/dashboard.jsx
@@ -1,42 +1,19 @@
-import React, {useEffect, useRef} from "react";
+import React from 'react';
 import './dashboard.css';
 
 import { FaTelegramPlane, FaInstagram, FaFacebook } from "react-icons/fa";
 
 function DashboardFunc() {
 
-    const videoRef = useRef(null)
-
-    useEffect(() => {
-        if (videoRef.current) {
-            videoRef.current.playbackRate = 0.4;
-        }
-    }, []);
-
     // React qismi bilan ishlash
     return (
         <div className="dashboardContainr">
-
-            {/* Background qismi bilan ishlash */}
-            <video ref={videoRef} autoPlay loop muted playsInline className="video-background">
-                <source src={require('../imgVideos/background.mp4')} type="video/mp4"/>
-                Sizning brauzeringiz videoni qo'llab-quvvatlamaydi.
-            </video>
-            <div className="content">
-                {/* Sizning dasturnizning kontenti shu yerda bo'ladi */}
-            </div>
 
             {/* Saytning ichki qismi bilan ishlash */}
             <div className="main-text-part">
 
                 {/* Messangers link */}
                 <div className="main-site-part-of-links">
-
-                    <div className="sindicate-text-part">
-                        <h2 className="sindicate-text">
-                            Спортивные товары Ташкент
-                        </h2>
-                    </div>
 
                     {/* Telegram link parts */}
                     <div className="messangers-link-part">
